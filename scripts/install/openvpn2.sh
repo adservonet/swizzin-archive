e2c32ef90917e79388ea4d2f1a05d2c453593c54
root <root@test.earnest.ltd>
2019-01-16 10:18:22 -0500

    base openvpn2 package

---
+++ scripts/install/openvpn2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# OpenVPN Headless install for box behind NAT
+# Author: Earnest
+
+if [[ -f /tmp/.install.lock ]]; then
+  log="/root/logs/install.log"
+elif [[ -f /install/.panel.lock ]]; then
+  log="/srv/panel/db/output.log"
+else
+  log="/dev/null"
+fi
+
+curl -O https://raw.githubusercontent.com/madeinearnest/openvpn-install/master/openvpn-install.sh
+chmod +x openvpn-install.sh
+
+./openvpn-install.sh
+
+touch /install/.openvpn2.lock
