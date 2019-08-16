ea91053165b9f391ec6ff61015ea4e1db074e5b4
sbrendel <illnesse@gmail.com>
2019-08-16 22:32:55 +0200

    nginx

--- scripts/nginx/seedit4me.sh
+++ scripts/nginx/seedit4me.sh
@@ -48,7 +48,7 @@ location ~ \.php$
     include snippets/fastcgi-php.conf;
     fastcgi_pass unix:/run/php/$sock.sock;
     #fastcgi_index index.php;
-    fastcgi_param SCRIPT_FILENAME /srv/seedit4me\$fastcgi_script_name;
+    fastcgi_param SCRIPT_FILENAME /srv\$fastcgi_script_name;
   }
 }
 
