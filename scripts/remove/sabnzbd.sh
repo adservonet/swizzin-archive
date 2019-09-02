e7de4fafb0cff75eb1e9cc62208f08616fab641f
sbrendel <illnesse@gmail.com>
2019-09-02 21:45:11 +0200

    remove ini too

--- scripts/remove/sabnzbd.sh
+++ scripts/remove/sabnzbd.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 username=$(cut -d: -f1 < /root/.master.info)
 
+
+
 apt-get -y remove par2-tbb python-openssl python-sabyenc python-cheetah >/dev/null 2>&1
 rm -rf /home/$username/SABnzbd
+rm -rf /home/$username/.sabnzbd
 systemctl disable sabnzbd@$username
 systemctl stop sabnzbd@$username
 rm /etc/systemd/system/sabnzbd@.service
