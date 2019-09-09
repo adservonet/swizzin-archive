cbd9fa4903a7b9a139224522e17a4d501564e9ee
illnesse <illnesse@gmail.com>
2019-09-09 12:00:11 +0200

    lazylibrarian

--- scripts/remove/lazylibrarian.sh
+++ scripts/remove/lazylibrarian.sh
@@ -8,6 +8,6 @@ systemctl disable lazylibrarian@${MASTER}.service >/dev/null 2>&1
 rm /etc/systemd/system/lazylibrarian.service
 
 rm -rf /srv/lazylibrarian
-rm -rf /etc/nginx/apps/pyload.conf
-rm /install/.pyload.lock
+rm -rf /etc/nginx/apps/lazylibrarian.conf
+rm /install/.lazylibrarian.lock
 
