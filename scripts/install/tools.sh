2cb296d3da69999e6007ca8632fdf92dcc2e4490
illnesse <illnesse@gmail.com>
2019-09-09 17:38:17 +0200

    umm wrong spot actually

--- scripts/install/tools.sh
+++ scripts/install/tools.sh
@@ -17,12 +17,5 @@ if [[ ! -f /install/.nginx.lock ]]; then
   exit 1
 fi
 
-apt -y -q  remove php7.0 php7.0-fpm php7.0-cli php7.0-common
-LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-apt -y -q  update
-apt -y -q  install nano php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-zip php7.3-sqlite3 php7.3-curl php7.3-simplexml
-update-alternatives --set php /usr/bin/php7.3
-sed -i "s/php7.0-fpm/php7.3-fpm/g" /etc/nginx/apps/*.conf
-
 bash /usr/local/bin/swizzin/nginx/tools.sh
 touch /install/.tools.lock
--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -2,6 +2,14 @@
 
 if [[ -d /srv/tools ]]; then
   echo "Updating tools"
+
+  apt -y -q  remove php7.0 php7.0-fpm php7.0-cli php7.0-common
+  LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+  apt -y -q  update
+  apt -y -q  install nano php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-zip php7.3-sqlite3 php7.3-curl php7.3-simplexml
+  update-alternatives --set php /usr/bin/php7.3
+  sed -i "s/php7.0-fpm/php7.3-fpm/g" /etc/nginx/apps/*.conf
+
   cp -r /usr/local/bin/swizzin/tools/php/* /srv/tools/
 
   chown -R www-data: /srv/tools
