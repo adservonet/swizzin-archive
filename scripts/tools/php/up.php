e84552e26f5827011078c1e91107cc87f6d89d2e
illnesse <illnesse@gmail.com>
2019-09-09 15:56:44 +0200

    php 7.3 fixes

--- scripts/tools/php/up.php
+++ scripts/tools/php/up.php
@@ -2,9 +2,9 @@
 include ('cors.php');
 $uptime = shell_exec("cut -d. -f1 /proc/uptime");
 $days = floor($uptime/60/60/24);
-$hours = $uptime/60/60%24;
-$mins = $uptime/60%60;
-$secs = $uptime%60;
+$hours = int($uptime)/60/60%24;
+$mins = int($uptime)/60%60;
+$secs = int($uptime)%60;
 echo $days."$".$hours."$".$mins."$".$secs;
 ?>
 
