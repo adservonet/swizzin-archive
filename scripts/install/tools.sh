7331f0e6c647cb7ab67cc4999f2f7d42704afdf4
sbrendel <illnesse@gmail.com>
2019-09-09 21:51:37 +0200

    php 7.3 on tools install

--- scripts/install/tools.sh
+++ scripts/install/tools.sh
@@ -12,6 +12,17 @@
 
 log="/dev/null"
 
+if [[ -f /lib/systemd/system/php7.3-fpm.service ]]; then
+  echo "php setup seems ok"
+else
+  apt -y -q  remove php7.0 php7.0-fpm php7.0-cli php7.0-common
+  LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+  apt -y -q  update
+  apt -y -q  install nano php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-zip php7.3-sqlite3 php7.3-curl php7.3-simplexml
+  update-alternatives --set php /usr/bin/php7.3
+  sed -i "s/php7.0-fpm/php7.3-fpm/g" /etc/nginx/apps/*.conf
+fi
+
 if [[ ! -f /install/.nginx.lock ]]; then
   echo "ERROR: Web server not detected. Please install nginx and restart seedit4me install."
   exit 1
