0a2d3d7771119c8eac7f3c0415d7529340e58e3b
sbrendel <illnesse@gmail.com>
2019-09-11 20:24:39 +0200

    touch and chmod

--- scripts/tools/php/deletelogs.php
+++ scripts/tools/php/deletelogs.php
@@ -1,5 +1,7 @@
 <?php
 //header('Content-Type: application/json');
 shell_exec("rm /srv/tools/logs/*");
+shell_exec("touch /srv/tools/logs/output.log");
+shell_exec("chmod 777 /srv/tools/logs/output.log");
 echo "logs deleted!";
 ?>
