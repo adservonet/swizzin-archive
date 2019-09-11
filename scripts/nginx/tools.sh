aa7fa2ddf62f7774ddb60d8621caff37d2f6ffcc
illnesse <illnesse@gmail.com>
2019-09-11 14:37:05 +0200

    all methods

--- scripts/nginx/tools.sh
+++ scripts/nginx/tools.sh
@@ -71,13 +71,13 @@ location ~ \.php$
   }
 }
 
-location /tools/logs {
-alias /srv/tools/logs ;
+location /tools/logs/ {
+alias /srv/tools/logs/ ;
 allow all;
 
 add_header 'Access-Control-Allow-Origin' '*' always;
 add_header 'Access-Control-Allow-Credentials' 'true';
-add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+add_header 'Access-Control-Allow-Methods' '*';
 add_header 'Access-Control-Allow-Headers' 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,accept,origin,X-Requested-With,X-CSRF-Token' always;
 add_header 'Cache-Control' 'no-store, no-cache, must-revalidate';
 }
