a6099badf94031ceee9727969800e9783fa34534
sbrendel <illnesse@gmail.com>
2019-09-11 23:46:34 +0200

    deluser

--- scripts/remove/tautulli.sh
+++ scripts/remove/tautulli.sh
@@ -4,5 +4,6 @@ systemctl disable tautulli
 rm -rf /opt/tautulli
 rm /install/.tautulli.lock
 rm -f /etc/nginx/apps/tautulli.conf
+deluser --force --remove-home tautulli
 service nginx reload
 rm /etc/systemd/system/tautulli.service
