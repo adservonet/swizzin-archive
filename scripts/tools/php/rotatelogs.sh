1b0ea6120aa34c26af52e82a6f7672318b8b3899
sbrendel <illnesse@gmail.com>
2019-09-12 22:51:47 +0200

    better cp

--- scripts/tools/php/rotatelogs.sh
+++ scripts/tools/php/rotatelogs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-mv /srv/tools/logs/output.log /srv/tools/logs/output_$(date +%F-%H:%M:%S).log
+cp /srv/tools/logs/output.log /srv/tools/logs/output_$(date +%F-%H_%M_%S).log
+sleep 1s
+rm /srv/tools/logs/output.log
 touch /srv/tools/logs/output.log
 chmod 777 /srv/tools/logs/output.log
