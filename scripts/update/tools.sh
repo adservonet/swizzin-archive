0ce9a997441b6a79534188b0d1608865a4f660f0
sbrendel <illnesse@gmail.com>
2019-09-14 21:49:47 +0200

    always install tools

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-if [[ -d /srv/tools ]]; then
+#always install tools
+#if [[ -d /srv/tools ]]; then
   echo "Updating tools"
 
   /usr/local/bin/swizzin/remove/tools.sh
   /usr/local/bin/swizzin/install/tools.sh
-
-fi
+#fi
