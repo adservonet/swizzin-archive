ca3b710d548c33a346af20e78deff345cacba7eb
illnesse <illnesse@gmail.com>
2019-09-17 12:18:28 +0200

    emby test

--- scripts/upgrade/emby.sh
+++ scripts/upgrade/emby.sh
@@ -4,5 +4,5 @@
 current=$(curl -L -s -H 'Accept: application/json' https://github.com/MediaBrowser/Emby.Releases/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 cd /tmp
 wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby.Releases/releases/download/${current}/emby-server-deb_${current}_amd64.deb
-dpkg -i emby.dpkg >> /dev/null 2>&1
+dpkg -i emby.dpkg
 rm emby.dpkg
