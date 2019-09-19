19a472f7b055d50f5386828da2aa6c15a3b86835
sbrendel <illnesse@gmail.com>
2019-09-19 21:26:23 +0200

    waitforapt KILL KILL KILL

--- sources/functions/waitforapt.sh
+++ sources/functions/waitforapt.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 function waitforapt {
+  i=0
+  echo i$i
+  echo "waiting 30 seconds for apt locks, before i kill apt-get"  >> "${SEEDIT_LOG}" 2>&1;
   while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-    echo "waiting for apt locks"
-    echo "waiting for apt locks" >> "${SEEDIT_LOG}" 2>&1;
+    ((i++))
+    echo $i >> "${SEEDIT_LOG}" 2>&1;
+
+    if [ $i == 30 ]
+    then
+      echo "killall apt-get" >> "${SEEDIT_LOG}" 2>&1;
+      echo "waiting another 30 seconds till i delete locks" >> "${SEEDIT_LOG}" 2>&1;
+      sudo killall apt-get >> "${SEEDIT_LOG}" 2>&1;
+    fi
+
+    if [ $i == 60 ]
+    then
+      echo "deleting locks" >> "${SEEDIT_LOG}" 2>&1;
+      sudo rm /var/lib/dpkg/lock
+      sudo rm /var/lib/dpkg/lock-frontend
+      sudo rm /var/cache/apt/archives/lock
+      sudo rm /var/cache/debconf/*.dat
+    fi
+
     sleep 1
   done
 }
