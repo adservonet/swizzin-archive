6fb1f85fe368c04880fa03d63812c1b1efa937bc
sbrendel <illnesse@gmail.com>
2019-09-20 13:32:05 +0200

    count space from root test

--- scripts/tools/php/disk_data.php
+++ scripts/tools/php/disk_data.php
@@ -52,9 +52,9 @@ try {
 }
 
 //hard disk
-$dftotal = (round(@disk_total_space(".")/(1024*1024*1024),1)); //Total
-$dffree = (round(@disk_free_space(".")/(1024*1024*1024),1)); //Available
-$dfused = (round(@disk_total_space(".")/(1024*1024*1024),1)-round(@disk_free_space(".")/(1024*1024*1024),1)); //used
+$dftotal = (round(@disk_total_space("/")/(1024*1024*1024),1)); //Total
+$dffree = (round(@disk_free_space("/")/(1024*1024*1024),1)); //Available
+$dfused = (round(@disk_total_space("/")/(1024*1024*1024),1)-round(@disk_free_space("/")/(1024*1024*1024),1)); //used
 
 echo $dffree ."$". $dfused ."$". $dftotal
 ?>
