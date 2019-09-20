b2e7f170ad1b889cb3c463304f19f2b77b680993
sbrendel <illnesse@gmail.com>
2019-09-20 12:06:39 +0200

    echo to console

--- sources/functions/waitforapt.sh
+++ sources/functions/waitforapt.sh
@@ -3,20 +3,25 @@
 function waitforapt {
   i=0
   echo i$i
+  echo "waiting for apt locks"
   echo "waiting for apt locks"  >> "${SEEDIT_LOG}" 2>&1;
   while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
     ((i++))
+    echo $i
     echo $i >> "${SEEDIT_LOG}" 2>&1;
 
     if [ $i == 30 ]
     then
+      echo "killall apt-get"
       echo "killall apt-get" >> "${SEEDIT_LOG}" 2>&1;
+      echo "waiting another 30 seconds till i delete locks"
       echo "waiting another 30 seconds till i delete locks" >> "${SEEDIT_LOG}" 2>&1;
       sudo killall apt-get >> "${SEEDIT_LOG}" 2>&1;
     fi
 
     if [ $i == 60 ]
     then
+      echo "deleting locks"
       echo "deleting locks" >> "${SEEDIT_LOG}" 2>&1;
       sudo rm /var/lib/dpkg/lock
       sudo rm /var/lib/dpkg/lock-frontend
