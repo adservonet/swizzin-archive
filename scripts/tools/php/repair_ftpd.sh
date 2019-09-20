96f091aa72f3c626eefe3f6909a1629abcdc1046
sbrendel <illnesse@gmail.com>
2019-09-20 12:17:17 +0200

    added logging

--- scripts/tools/php/repair_ftpd.sh
+++ scripts/tools/php/repair_ftpd.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if [[ -f /install/.tools.lock ]]; then
+  export SEEDIT_LOG=/srv/tools/logs/output.log
+else
+  export SEEDIT_LOG=/root/logs/install.log
+fi
+
 . /etc/swizzin/sources/functions/waitforapt.sh
 waitforapt
-apt remove proftpd proftpd-basic vsftpd;
+
+apt remove proftpd proftpd-basic vsftpd >> "${SEEDIT_LOG}" 2>&1
 
 rm /install/.proftpd.lock
 rm /install/.vsftpd.lock
 
-. /etc/swizzin/sources/functions/waitforapt.sh
 waitforapt
 
-box install proftpd;
+box install proftpd
