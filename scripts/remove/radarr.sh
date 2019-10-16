b6fa33581242ac8b1dd3c5e0881ce646877d5e53
sbrendel <illnesse@gmail.com>
2019-10-16 20:27:37 +0200

    delete radarr confs

--- scripts/remove/radarr.sh
+++ scripts/remove/radarr.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+MASTER=$(cut -d: -f1 < /root/.master.info)
 systemctl stop radarr
 systemctl disable radarr
+rm -rf /home/${MASTER}/.config/Radarr
 rm -rf /etc/systemd/system/radarr.service
 rm -rf /opt/Radarr
 rm -rf /etc/nginx/apps/radarr.conf
