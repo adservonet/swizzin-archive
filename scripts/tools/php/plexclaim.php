139931b9d5f7a47d17693cc2968c67b1469c2ef9
sbrendel <illnesse@gmail.com>
2019-10-17 16:02:51 +0200

    out

--- scripts/tools/php/plexclaim.php
+++ scripts/tools/php/plexclaim.php
@@ -11,7 +11,7 @@ if (isset($_POST['token']))
 
 if ($token != null)
 {
-    exec("sudo -u plex /srv/tools/plexclaim.sh " . $token,$out);
+    $out = trim(shell_exec("sudo -u plex /srv/tools/plexclaim.sh " . $token));
     var_dump($out);
 }
 ?>
