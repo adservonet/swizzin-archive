41ffb82b84f910af830fe257dc44b888feb7d304
sbrendel <illnesse@gmail.com>
2019-10-17 16:28:22 +0200

    testing plex claim

--- scripts/tools/php/plexclaim.php
+++ scripts/tools/php/plexclaim.php
@@ -12,6 +12,7 @@ if (isset($_POST['token']))
 if ($token != null)
 {
     $out = trim(shell_exec("sudo -u plex /srv/tools/plexclaim.sh " . $token));
+    echo $out;
     if (strpos($out, 'success') !== false) {
         echo "Plex server claimed successfully using token ".$token;
     }
