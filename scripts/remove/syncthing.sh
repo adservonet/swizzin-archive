8a738d5007ad399642c2c56b1333497a699aee26
sbrendel <illnesse@gmail.com>
2019-10-25 00:06:13 +0200

    waitforapt in remove

--- scripts/remove/syncthing.sh
+++ scripts/remove/syncthing.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 MASTER=$(cut -d: -f1 < /root/.master.info)
+
+. /etc/swizzin/sources/functions/waitforapt.sh
+
 systemctl stop syncthing@${MASTER}
+
+waitforapt
 apt-get -q -y purge syncthing
 rm /etc/systemd/system/syncthing@.service
 rm -f  /etc/nginx/apps/syncthing.conf
