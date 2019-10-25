37c08886344b2962000aa39837fb2baa0ce8413e
sbrendel <illnesse@gmail.com>
2019-10-25 21:45:39 +0200

    install fuse

--- scripts/install/rclone.sh
+++ scripts/install/rclone.sh
@@ -24,6 +24,15 @@
 #fi
 MASTER=$(cut -d: -f1 < /root/.master.info)
 
+. /etc/swizzin/sources/functions/waitforapt.sh
+waitforapt
+
+apt-get update -y -q
+
+waitforapt
+
+apt-get install -y fuse >>  "${SEEDIT_LOG}"  2>&1
+
 echo "Downloading and installing rclone ..." >> "${SEEDIT_LOG}"  2>&1;
 
 # One-liner to check arch/os type, as well as download latest rclone for relevant system.
