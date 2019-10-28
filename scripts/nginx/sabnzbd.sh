d2c1c2da12b656bd8df8dc583b700f4d6127e758
sbrendel <illnesse@gmail.com>
2019-10-28 19:09:40 +0100

    disable sabnzbd nginx auth

--- scripts/nginx/sabnzbd.sh
+++ scripts/nginx/sabnzbd.sh
@@ -14,8 +14,8 @@ if [[ ! -f /etc/nginx/apps/sabnzbd.conf ]]; then
 location /sabnzbd {
   include /etc/nginx/snippets/proxy.conf;
   proxy_pass        http://127.0.0.1:65080/sabnzbd;
-  auth_basic "What's the password?";
-  auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
+  #auth_basic "What's the password?";
+  #auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
 }
 SAB
 fi
