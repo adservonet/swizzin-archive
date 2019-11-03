4043107eb46dea3b55e5201b89ad761f5cc62560
sbrendel <illnesse@gmail.com>
2019-11-03 21:15:15 +0100

    --force-confnew

--- scripts/upgrade/emby.sh
+++ scripts/upgrade/emby.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 # Simple tool to grab the latest release of emby
 
-. /etc/swizzin/sources/functions/waitforapt.sh
-
 current=$(curl -L -s -H 'Accept: application/json' https://github.com/MediaBrowser/Emby.Releases/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 cd /tmp
 wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby.Releases/releases/download/${current}/emby-server-deb_${current}_amd64.deb  >> "${SEEDIT_LOG}"  2>&1;
 
+. /etc/swizzin/sources/functions/waitforapt.sh
 waitforapt
 
-echo 'Dpkg::Options {\n"--force-confnew";\n};' > /etc/apt/apt.conf.d/71debconf;
-export DEBIAN_FRONTEND=noninteractive
-
-dpkg -i emby.dpkg  >> "${SEEDIT_LOG}"  2>&1;
+dpkg --force-confnew -i emby.dpkg  >> "${SEEDIT_LOG}"  2>&1;
 rm emby.dpkg
