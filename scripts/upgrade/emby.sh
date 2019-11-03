80a96c5077b1107d9a1edf80f67214d87662b1f6
sbrendel <illnesse@gmail.com>
2019-11-03 21:24:07 +0100

    chmod 777 /var/lib/emby/logs

--- scripts/upgrade/emby.sh
+++ scripts/upgrade/emby.sh
@@ -8,5 +8,7 @@ wget -q -O emby.dpkg https://github.com/MediaBrowser/Emby.Releases/releases/down
 . /etc/swizzin/sources/functions/waitforapt.sh
 waitforapt
 
+chmod 777 /var/lib/emby/logs
+
 dpkg --force-confnew -i emby.dpkg  >> "${SEEDIT_LOG}"  2>&1;
 rm emby.dpkg
