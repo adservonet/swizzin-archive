41569853e4d649a61bdb794d796007b268114463
sbrendel <illnesse@gmail.com>
2019-11-12 19:02:26 +0100

    tools update

--- scripts/remove/tools.sh
+++ scripts/remove/tools.sh
@@ -3,7 +3,7 @@
 #rm -rf /srv/tools
 rm /srv/tools/*.*
 
-if [[ ! -f /srv/tools/vendor ]]; then
+if [[ ! -d /srv/tools/vendor ]]; then
   rm -rf /srv/tools/vendor
 fi
 
@@ -11,7 +11,7 @@ if [[ ! -f /etc/nginx/apps/tools.conf ]]; then
   rm -f /etc/nginx/apps/tools.conf
 fi
 
-if [[ ! -f /etc/sudoers.d/tools ]]; then
+if [[ ! -d /etc/sudoers.d/tools ]]; then
   rm -f /etc/sudoers.d/tools
 fi
 
