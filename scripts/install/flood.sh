af724c1f84d0a3c3b807ef4dc5a9f81ba582f83c
sbrendel <illnesse@gmail.com>
2019-11-15 01:38:22 +0100

    flood

--- scripts/install/flood.sh
+++ scripts/install/flood.sh
@@ -18,10 +18,6 @@ fi
 . /etc/swizzin/sources/functions/npm
 npm_install
 
-if [[ ! $(which node-gyp) ]]; then
-  npm install -g node-gyp >>  "${SEEDIT_LOG}"  2>&1
-fi
-
 cat > /etc/systemd/system/flood@.service <<SYSDF
 [Unit]
 Description=Flood rTorrent Web UI
@@ -30,45 +26,36 @@ After=network.target
 [Service]
 User=%I
 Group=%I
-WorkingDirectory=/home/%I/.flood
-ExecStart=/usr/bin/npm start --production /home/%I/.flood
+WorkingDirectory=/srv/flood
+ExecStart=/usr/bin/npm start
 
 
 [Install]
 WantedBy=multi-user.target
 SYSDF
 
-users=($(cut -d: -f1 < /etc/htpasswd))
-for u in "${users[@]}"; do
-  if [[ ! -d /home/$u/.flood ]]; then
-    salt=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 32 | head -n 1)
-    port=$(shuf -i 3501-4500 -n 1)
-    cd /home/$u
-    git clone https://github.com/jfurrow/flood.git .flood >>  "${SEEDIT_LOG}"  2>&1
-    chown -R $u: .flood
-    cd .flood
-    cp -a config.template.js config.js
-    sed -i "s/floodServerPort: 3000/floodServerPort: $port/g" config.js
-    sed -i "s/socket: false/socket: true/g" config.js
-    sed -i "s/socketPath.*/socketPath: '\/var\/run\/${u}\/.rtorrent.sock'/g" config.js
-    sed -i "s/secret: 'flood'/secret: '$salt'/g" config.js
-    if [[ ! -f /install/.nginx.lock ]]; then
-      sed -i "s/floodServerHost: '127.0.0.1'/floodServerHost: '0.0.0.0'/g" config.js
-    fi
-    echo "Building Flood for $u. This might take some time..."
-    echo ""
-    su - $u -c "cd /home/$u/.flood; npm install" >>  "${SEEDIT_LOG}"  2>&1
-    if [[ ! -f /install/.nginx.lock ]]; then
-      su - $u -c "cd /home/$u/.flood; npm run build" >>  "${SEEDIT_LOG}"  2>&1
-      systemctl start flood@$u
-      echo "Flood port for $u is $port"
-    elif [[ -f /install/.nginx.lock ]]; then    
-      bash /usr/local/bin/swizzin/nginx/flood.sh $u
-      systemctl start flood@$u
-    fi
-    systemctl enable flood@$u > /dev/null 2>&1
-  fi
-done
+user=$(cut -d: -f1 < /root/.master.info)
+salt=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 32 | head -n 1)
+cd /srv
+git clone https://github.com/jfurrow/flood.git .flood >>  "${SEEDIT_LOG}"  2>&1
+#chown -R $u: flood
+cp -a config.template.js config.js
+sed -i "s/baseURI: '/'/baseURI: '/flood'/g" config.js
+sed -i "s/floodServerPort: 3000/floodServerPort: 3001/g" config.js
+sed -i "s/socket: false/socket: true/g" config.js
+sed -i "s/socketPath.*/socketPath: '\/var\/run\/${user}\/.rtorrent.sock'/g" config.js
+sed -i "s/secret: 'flood'/secret: '$salt'/g" config.js
+echo "Building Flood for $u. This might take some time..." >>  "${SEEDIT_LOG}"  2>&1
+cd /srv/flood
+npm install >>  "${SEEDIT_LOG}"  2>&1
+npm install -g node-gyp >>  "${SEEDIT_LOG}"  2>&1
+npm run build >>  "${SEEDIT_LOG}"  2>&1
+
+if [[ -f /install/.nginx.lock ]]; then
+  bash /usr/local/bin/swizzin/nginx/flood.sh
+  systemctl start flood
+fi
+systemctl enable flood > /dev/null 2>&1
 
 
 
