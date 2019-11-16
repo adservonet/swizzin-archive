6e37910b6fef2cab1fa09aa785f82890f3fe34e3
sbrendel <illnesse@gmail.com>
2019-11-17 00:13:37 +0100

    notify.sh

--- scripts/tools/php/notify.sh
+++ scripts/tools/php/notify.sh
@@ -1,17 +1,17 @@
+#!/bin/bash
 hostname="$(cat /proc/sys/kernel/hostname)"
 hostname="${hostname/ct/}"
 slackurl="https://hooks.dudewtf/services/TLKMV1Z3R/BQM2PJ2SG/cddPy3W3ILDmQK0brEUQTf2U"
 slackurl="${slackurl/dudewtf/slack.com}"
 
 cat /tmp/rutorrent_errors.log | grep "public tracker" | while read -r line ; do
-    echo $slackurl;
     curl -X POST -H 'Content-type: application/json' --data "$(cat <<EOF
 {
 "text": "<https://my.seedit4.me/boxes/$hostname|$hostname> : $line"
 }
 EOF
-)" ${slackurl}
-done
+)" $slackurl
+done;
 
 rm /tmp/rutorrent_errors.log
 
