00032870d0e1bf5db4213bdb68741305884317a1
sbrendel <illnesse@gmail.com>
2019-11-16 23:55:14 +0100

    url fix

--- scripts/tools/php/notify.sh
+++ scripts/tools/php/notify.sh
@@ -1,14 +1,17 @@
 hostname="$(cat /proc/sys/kernel/hostname)"
 hostname="${hostname/ct/}"
-slackurl="${https://hooks.dudewtf/services/TLKMV1Z3R/BQM2PJ2SG/cddPy3W3ILDmQK0brEUQTf2U/dudewtf/slack.com}"
+slackurl="https://hooks.dudewtf/services/TLKMV1Z3R/BQM2PJ2SG/cddPy3W3ILDmQK0brEUQTf2U"
+slackurl="${slackurl/dudewtf/slack.com}"
 
 cat /tmp/rutorrent_errors.log | grep "public tracker" | while read -r line ; do
+    echo $slackurl;
     curl -X POST -H 'Content-type: application/json' --data "$(cat <<EOF
 {
 "text": "<https://my.seedit4.me/boxes/$hostname|$hostname> : $line"
 }
 EOF
-)" $slackurl
+)" ${slackurl}
 done
 
 rm /tmp/rutorrent_errors.log
+
