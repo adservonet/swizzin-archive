cf2900753fe817125606c7ddbc632dacfa41073a
sbrendel <illnesse@gmail.com>
2019-12-08 00:39:48 +0100

    why sudo

--- scripts/install/sonarr3.sh
+++ scripts/install/sonarr3.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 waitforapt
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF >>  "${SEEDIT_LOG}"  2>&1
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF >>  "${SEEDIT_LOG}"  2>&1
 waitforapt
-sudo apt -y install apt-transport-https ca-certificates >>  "${SEEDIT_LOG}"  2>&1
+apt -y install apt-transport-https ca-certificates >>  "${SEEDIT_LOG}"  2>&1
 echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list >>  "${SEEDIT_LOG}"  2>&1
 waitforapt
-sudo apt update >>  "${SEEDIT_LOG}"  2>&1
+apt update >>  "${SEEDIT_LOG}"  2>&1
 
 waitforapt
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8 >>  "${SEEDIT_LOG}"  2>&1
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8 >>  "${SEEDIT_LOG}"  2>&1
 echo "deb https://apt.sonarr.tv/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/sonarr.list >>  "${SEEDIT_LOG}"  2>&1
 waitforapt
-sudo apt update
+apt update
 waitforapt
 export DEBIAN_FRONTEND=noninteractive
-sudo apt -y -f install sonarr >>  "${SEEDIT_LOG}"  2>&1
+apt -y -f install sonarr >>  "${SEEDIT_LOG}"  2>&1
 
 systemctl stop sonarr
 
--- scripts/remove/sonarr3.sh
+++ scripts/remove/sonarr3.sh
@@ -2,8 +2,8 @@
 
 systemctl stop sonarr
 systemctl disable sonarr
-sudo apt-get remove -y sonarr >/dev/null 2>&1
-sudo apt-get -y autoremove >/dev/null 2>&1
+apt-get remove -y sonarr >/dev/null 2>&1
+apt-get -y autoremove >/dev/null 2>&1
 rm -f /etc/apt/sources.list.d/sonarr.list
 rm -f /etc/nginx/apps/sonarr3.conf
 rm -rf /var/lib/sonarr/
