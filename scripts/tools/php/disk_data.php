b8e91994b5bb610e052679e7f1df34cabc211998
sbrendel <illnesse@gmail.com>
2020-05-02 21:09:36 +0200

    count space in /home only
    (cherry picked from commit 871ee160f0be59090ecf50ae0460fe6241057502)

--- scripts/tools/php/disk_data.php
+++ scripts/tools/php/disk_data.php
@@ -3,9 +3,9 @@ include('cors.php');
 header('Content-Type: text/plain');
 
 //hard disk
-$dftotal = (round(@disk_total_space("/")/(1024*1024*1024),1)); //Total
-$dffree = (round(@disk_free_space("/")/(1024*1024*1024),1)); //Available
-$dfused = (round(@disk_total_space("/")/(1024*1024*1024),1)-round(@disk_free_space("/")/(1024*1024*1024),1)); //used
+$dftotal = (round(@disk_total_space("/home")/(1024*1024*1024),1)); //Total
+$dffree = (round(@disk_free_space("/home")/(1024*1024*1024),1)); //Available
+$dfused = (round(@disk_total_space("/home")/(1024*1024*1024),1)-round(@disk_free_space("/")/(1024*1024*1024),1)); //used
 
 echo $dffree ."$". $dfused ."$". $dftotal
 ?>
