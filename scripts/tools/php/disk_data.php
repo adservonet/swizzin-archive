9180e778d767371f425e65b0525f0cb1d6e4ac3c
sbrendel <illnesse@gmail.com>
2020-05-22 12:10:41 +0200

    disk data $dir

--- scripts/tools/php/disk_data.php
+++ scripts/tools/php/disk_data.php
@@ -2,10 +2,12 @@
 include('cors.php');
 header('Content-Type: text/plain');
 
+$dir = "/home";  //"/";
+
 //hard disk
-$dftotal = (round(@disk_total_space("/home")/(1024*1024*1024),1)); //Total
-$dffree = (round(@disk_free_space("/home")/(1024*1024*1024),1)); //Available
-$dfused = (round(@disk_total_space("/home")/(1024*1024*1024),1)-round(@disk_free_space("/")/(1024*1024*1024),1)); //used
+$dftotal = (round(@disk_total_space($dir)/(1024*1024*1024),1)); //Total
+$dffree = (round(@disk_free_space($dir)/(1024*1024*1024),1)); //Available
+$dfused = (round(@disk_total_space($dir)/(1024*1024*1024),1)-round(@disk_free_space($dir)/(1024*1024*1024),1)); //used
 
 echo $dffree ."$". $dfused ."$". $dftotal
 ?>
