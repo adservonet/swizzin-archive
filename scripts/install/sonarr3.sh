ff44e237f8ac7aa48cb7f77f620b2b284d85f95d
illnesse <illnesse@gmail.com>
2020-06-12 01:14:57 +0200

    sonarr3 user fix?

--- scripts/install/sonarr3.sh
+++ scripts/install/sonarr3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+username=$(cut -d: -f1 < /root/.master.info)
 
 waitforapt
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF >>  "${log}"  2>&1
@@ -21,12 +22,39 @@ apt update
 waitforapt
 apt -y -f install sonarr >>  "${log}"  2>&1
 usermod -a -G seedit4me sonarr
-
+usermod -a -G sonarr seedit4me
 systemctl stop sonarr
+
+mv /lib/systemd/system/sonarr.service /lib/systemd/system/sonarr@.service
+
+chown -R "${username}":"${username}" /usr/lib/sonarr/
+
+cat > /lib/systemd/system/sonarr@.service <<SONARR
+[Unit]
+Description=Sonarr Daemon
+After=network.target
+
+[Service]
+User=%i
+Group=%i
+
+Type=forking
+ExecStart=/usr/bin/mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser -data=/var/lib/son$
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+SONARR
+
+  systemctl enable --now sonarr@${username} >> ${log} 2>&1
+  sleep 10
 
 touch /install/.sonarr3.lock
 
 if [[ -f /install/.nginx.lock ]]; then
+  sleep 3
   bash /usr/local/bin/swizzin/nginx/sonarr3.sh
-  service nginx reload
+  systemctl reload nginx
 fi
