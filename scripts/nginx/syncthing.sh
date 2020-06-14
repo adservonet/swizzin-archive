19750606a2e4af60cc7d7b504cb6fd71b451b841
illnesse <illnesse@gmail.com>
2020-06-14 17:55:16 +0200

    proxy_host

--- scripts/nginx/syncthing.sh
+++ scripts/nginx/syncthing.sh
@@ -13,7 +13,7 @@ if [[ ! -f /etc/nginx/apps/syncthing.conf ]]; then
 cat > /etc/nginx/apps/syncthing.conf <<SYNC
 location /syncthing/ {
   proxy_pass              http://127.0.0.1:8384/;
-  proxy_set_header        Host \$host;
+  proxy_set_header        Host \$proxy_host;
   proxy_set_header        X-Real-IP \$remote_addr;
   proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
   proxy_set_header        X-Forwarded-Proto \$scheme;
