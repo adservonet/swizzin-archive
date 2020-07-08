7ba1e7109bf0a098ccb940ffe7a495abbf7ac3cf
illnesse <illnesse@gmail.com>
2020-07-08 15:57:41 +0200

    --allow-unauthenticated

--- scripts/install/nginx.sh
+++ scripts/install/nginx.sh
@@ -45,7 +45,7 @@ apt-get -y -f install
 apt-get -y -qq update
 APT='nginx-extras subversion ssl-cert php7.3-fpm php7.3-common libfcgi0ldbl php7.3-cli php7.3-dev php7.3-xml php7.3-curl php7.3-xmlrpc php7.3-json php7.3-mbstring php7.3-opcache php-geoip php7.3-xml php7.3-gd'
 for depends in $APT; do
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install "$depends" >>  "${SEEDIT_LOG}"  2>&1 || { echo "ERROR: APT-GET could not install a required package: ${depends}. That's probably not good..."; }
+apt-get -y --allow-unauthenticated -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install "$depends" >>  "${SEEDIT_LOG}"  2>&1 || { echo "ERROR: APT-GET could not install a required package: ${depends}. That's probably not good..."; }
 done
 
 cd /etc/php
