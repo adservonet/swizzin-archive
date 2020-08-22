67e65a56e4944728a23ceda40c40bf1b7b10fe9a
illnesse <illnesse@gmail.com>
2020-08-22 10:26:52 +0200

    del lock

--- scripts/remove/qbittorrent.sh
+++ scripts/remove/qbittorrent.sh
@@ -9,5 +9,4 @@ apt-get remove -y qbittorrent-nox >>  "${log}"  2>&1
 userdel -r qbittorrent-nox
 
 rm -rf /etc/nginx/apps/qbittorrent.conf
-rm -rf /var/lib/transmission-daemon
-rm -rf /install/.transmission.lock
+rm -rf /install/.qbittorrent.lock
