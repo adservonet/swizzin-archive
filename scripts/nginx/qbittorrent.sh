62850e9915ae55184928e6a16416598230c6295b
illnesse <illnesse@gmail.com>
2020-08-22 10:47:49 +0200

    qbittorrent wip nginx

--- scripts/nginx/qbittorrent.sh
+++ scripts/nginx/qbittorrent.sh
@@ -10,12 +10,20 @@ location /qbittorrent {
     rewrite ^(.*[^/])$ $1/ redirect;
 }
 location ~ ^/qbittorrent/(?<url>.*) {
+    include /etc/nginx/snippets/proxy.conf;
+    proxy_set_header   X-Forwarded-Host   \$host;
+    proxy_set_header   X-Forwarded-Server \$host;
+    proxy_set_header   X-Forwarded-For    \$proxy_add_x_forwarded_for;
+
     proxy_pass http://127.0.0.1:9148/\$url;
-    include /config/proxy.conf;
     proxy_hide_header Referer;
     proxy_hide_header Origin;
     proxy_set_header Referer '';
     proxy_set_header Origin '';
+    add_header              X-Frame-Options         "SAMEORIGIN";
+
+    auth_basic "What's the password?";
+    auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
 }
 
 #location /qbittorrent {
