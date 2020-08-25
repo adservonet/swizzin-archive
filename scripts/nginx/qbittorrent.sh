468df3f2535b8e45b82af2670de52e163bfd5ca8
illnesse <illnesse@gmail.com>
2020-08-25 12:10:47 +0200

    qb conf nginx

--- scripts/nginx/qbittorrent.sh
+++ scripts/nginx/qbittorrent.sh
@@ -13,7 +13,6 @@ location ~ ^/qbittorrent/(?<url>.*) {
     proxy_set_header   X-Forwarded-Host   \$host;
     proxy_set_header   X-Forwarded-Server \$host;
     proxy_set_header   X-Forwarded-For    \$proxy_add_x_forwarded_for;
-    proxy_http_version      1.1;
     http2_push_preload on;
 
     proxy_pass http://127.0.0.1:9148/\$url;
