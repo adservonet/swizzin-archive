7944bdce20588546bd574ba435a5f9edaef64443
illnesse <illnesse@gmail.com>
2020-10-10 17:53:23 +0200

    apps.php

--- scripts/tools/php/apps.php
+++ scripts/tools/php/apps.php
@@ -91,7 +91,7 @@ $apps = array(
     array("name" => "medusa",         "service" => "medusa",            "user" => $username),
     array("name" => "netdata",        "service" => "netdata",           "user" => ""),
     array("name" => "sonarr",         "service" => "sonarr",            "user" => $username),
-    array("name" => "sonarrv3",       "service" => "sonarr",            "user" => $username),
+    array("name" => "sonarrv3",       "service" => "sonarr",            "user" => ""),
     array("name" => "subsonic",       "service" => "subsonic",          "user" => ""),
     array("name" => "syncthing",      "service" => "syncthing",         "user" => $username),
     array("name" => "jackett",        "service" => "jackett",           "user" => $username),
