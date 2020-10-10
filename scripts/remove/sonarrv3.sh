58ffa9e9a06429d23b04544555c6be671b1e948a
illnesse <illnesse@gmail.com>
2020-10-10 17:32:44 +0200

    sonarrv3 remove

--- scripts/remove/sonarrv3.sh
+++ scripts/remove/sonarrv3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 log="/root/logs/swizzin.log"
 
+systemctl stop sonarr
+systemctl disable sonarr
 apt_remove --purge sonarr
+rm -f /etc/apt/sources.list.d/sonarr.list
+
 
 rm -rf /var/lib/sonarr
 rm -rf /usr/lib/sonarr
