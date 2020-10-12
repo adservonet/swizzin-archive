e2cf52043f0cbd6cd3ab5603ee81185066b821d3
illnesse <illnesse@gmail.com>
2020-10-12 16:51:04 +0200

    transmission nginx

--- scripts/nginx/transmission.sh
+++ scripts/nginx/transmission.sh
@@ -14,51 +14,61 @@ location /transmission {
 }
 
 location /transmission/ {
-    proxy_set_header X-Real-IP \$remote_addr;
+    include /etc/nginx/snippets/proxy.conf;
+
+    proxy_pass_header  X-Transmission-Session-Id;
+    proxy_set_header   X-Forwarded-Host   \$host;
+    proxy_set_header   X-Forwarded-Server \$host;
     proxy_set_header   X-Forwarded-For    \$proxy_add_x_forwarded_for;
-    proxy_set_header Host \$http_host;
-    proxy_set_header X-NginX-Proxy true;
-    proxy_http_version 1.1;
-    proxy_set_header Connection "";
-    proxy_pass_header X-Transmission-Session-Id;
-    add_header   Front-End-Https   on;
+
+    proxy_pass        http://127.0.0.1:9091/transmission/web;
     auth_basic "What's the password?";
-    auth_basic_user_file /etc/htpasswd;
-    proxy_pass http://\$remote_user.transmission;
+    auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
 }
+
+location /rpc {
+    proxy_pass_header  X-Transmission-Session-Id;
+    proxy_pass         http://127.0.0.1:9091/transmission/rpc;
+}
+
+location /upload {
+    proxy_pass_header  X-Transmission-Session-Id;
+    proxy_pass         http://127.0.0.1:9091/transmission/upload;
+}
+
 TCONF
 fi
 
-for u in ${users[@]}; do
-    active=$(systemctl is-active transmission@$u)
-    if [[ $active == "active" ]]; then
-        systemctl stop transmission@${u}
-    fi
-
-    timeout=0
-    while systemctl is-active transmission@"$u" > /dev/null ;do
-        # echo "is active"
-        timeout+=0.3
-        if [[ $timeout -ge 20 ]]; then 
-            echo "The service transmission@$u took too long to shut down. Aborting."
-            exit 1
-        fi
-        sleep 0.3
-    done
-
-    confpath="/home/${u}/.config/transmission-daemon/settings.json"
-    jq '.["rpc-bind-address"] = "127.0.0.1"' "$confpath" >> /root/logs/swizzin.log
-    RPCPORT=$(jq -r '.["rpc-port"]' < "$confpath")
-    echo "RPC-port = $RPCPORT"
-    if [[ ! -f /etc/nginx/conf.d/${u}.transmission.conf ]]; then
-        cat > /etc/nginx/conf.d/${u}.transmission.conf <<TDCONF
-upstream ${u}.transmission {
-    server 127.0.0.1:${RPCPORT};
-}
-TDCONF
-    fi
-    if [[ $active == "active" ]]; then
-        systemctl start transmission@${u}
-fi
-done
+#for u in ${users[@]}; do
+#    active=$(systemctl is-active transmission@$u)
+#    if [[ $active == "active" ]]; then
+#        systemctl stop transmission@${u}
+#    fi
+#
+#    timeout=0
+#    while systemctl is-active transmission@"$u" > /dev/null ;do
+#        # echo "is active"
+#        timeout+=0.3
+#        if [[ $timeout -ge 20 ]]; then
+#            echo "The service transmission@$u took too long to shut down. Aborting."
+#            exit 1
+#        fi
+#        sleep 0.3
+#    done
+#
+#    confpath="/home/${u}/.config/transmission-daemon/settings.json"
+#    jq '.["rpc-bind-address"] = "127.0.0.1"' "$confpath" >> /root/logs/swizzin.log
+#    RPCPORT=$(jq -r '.["rpc-port"]' < "$confpath")
+#    echo "RPC-port = $RPCPORT"
+#    if [[ ! -f /etc/nginx/conf.d/${u}.transmission.conf ]]; then
+#        cat > /etc/nginx/conf.d/${u}.transmission.conf <<TDCONF
+#upstream ${u}.transmission {
+#    server 127.0.0.1:${RPCPORT};
+#}
+#TDCONF
+#    fi
+#    if [[ $active == "active" ]]; then
+#        systemctl start transmission@${u}
+#fi
+#done
 
