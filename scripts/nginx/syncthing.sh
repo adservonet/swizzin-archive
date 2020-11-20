7afdc3cc6310c9e69b6da632ccbbd14be837c4cd
illnesse <illnesse@gmail.com>
2020-11-20 23:25:10 +0100

    testing syncthing nginx conf

--- scripts/nginx/syncthing.sh
+++ scripts/nginx/syncthing.sh
@@ -13,12 +13,14 @@ if [[ ! -f /etc/nginx/apps/syncthing.conf ]]; then
 cat > /etc/nginx/apps/syncthing.conf <<SYNC
 location /syncthing/ {
   proxy_pass              http://127.0.0.1:8384/;
-  proxy_set_header        Host \$proxy_host;
+  proxy_set_header        Host \$host;
   proxy_set_header        X-Real-IP \$remote_addr;
   proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
   proxy_set_header        X-Forwarded-Proto \$scheme;
   auth_basic "What's the password?";
   auth_basic_user_file /etc/htpasswd.d/htpasswd.${MASTER};
+  proxy_read_timeout      600s;
+  proxy_send_timeout      600s;
 }
 SYNC
 fi
