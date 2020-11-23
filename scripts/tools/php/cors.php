3e458c6b5d275db40ca5b9cc661fab38572a4627
illnesse <illnesse@gmail.com>
2020-11-24 00:05:52 +0100

    cors

--- scripts/tools/php/cors.php
+++ scripts/tools/php/cors.php
@@ -1,4 +1,13 @@
 <?php
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//	header('Content-Type', 'application/json');
+	header('Access-Control-Allow-Origin', 'https://my.dev.seedit4.me');
+	header('Access-Control-Allow-Credentials', 'true');
+	header('Access-Control-Max-Age', '60');
+	header('Access-Control-Allow-Headers', 'AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2');
+	header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+}
 //if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') die();
 //header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Credentials: true');
