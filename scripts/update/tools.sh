f8dfad47fa395c540087a7fcc5ac52b12dda028c
illnesse <illnesse@gmail.com>
2020-11-23 22:54:07 +0100

    loop lines

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -2,8 +2,13 @@
 
 #always install tools
 #if [[ -d /srv/tools ]]; then
-  echo_info  /etc/swizzin/sources/logo/logo1
-  echo_info  /etc/swizzin/sources/logo/logo1 > "${log}"  2>&1;
+
+#!/bin/bash
+  input="/etc/swizzin/sources/logo/logo1"
+  while IFS= read -r line
+  do
+	echo_info  "$line"
+  done < "$input"
   /usr/local/bin/swizzin/remove/tools.sh
   /usr/local/bin/swizzin/install/tools.sh
 #fi
