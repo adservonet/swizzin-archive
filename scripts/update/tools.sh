aaf396e279253f85876792fd53477f9666cbe580
illnesse <illnesse@gmail.com>
2020-11-23 23:12:31 +0100

    spam

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -4,7 +4,7 @@
 #if [[ -d /srv/tools ]]; then
 
 #!/bin/bash
-  input="/etc/swizzin/sources/logo/logo3"
+  input="/etc/swizzin/sources/logo/logo1"
   while IFS= read -r line
   do
 	colorprint "${green}${bold} $line"
