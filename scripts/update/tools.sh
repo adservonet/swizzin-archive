423d9f291a14a87ee47e0313fb32e920fbdaf7a4
illnesse <illnesse@gmail.com>
2020-11-23 22:38:55 +0100

    i like logo1 best

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -2,7 +2,7 @@
 
 #always install tools
 #if [[ -d /srv/tools ]]; then
-  cat /etc/swizzin/sources/logo/logo2
+  cat /etc/swizzin/sources/logo/logo1
   cat /etc/swizzin/sources/logo/logo1 > "${log}"  2>&1;
   /usr/local/bin/swizzin/remove/tools.sh
   /usr/local/bin/swizzin/install/tools.sh
