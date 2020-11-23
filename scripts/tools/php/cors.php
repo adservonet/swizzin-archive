60fd395a428c78a6926945895f504f94556618be
illnesse <illnesse@gmail.com>
2020-11-24 00:22:39 +0100

    options

--- scripts/tools/php/cors.php
+++ scripts/tools/php/cors.php
@@ -1,13 +1,13 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 //	header('Content-Type', 'application/json');
 	header('Access-Control-Allow-Origin: ' . $_SERVER["HTTP_ORIGIN"]);
 	header('Access-Control-Allow-Credentials: true');
 	header('Access-Control-Max-Age: 60');
 	header('Access-Control-Allow-Headers: AccountKey,x-requested-with, x-csrf-token, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2');
 	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-}
+//}
 //if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') die();
 //header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Credentials: true');
