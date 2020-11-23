d1b22d2ddf25333006cf4f3b9e4426be89df2cff
illnesse <illnesse@gmail.com>
2020-11-23 22:56:25 +0100

    colorprint

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -7,7 +7,7 @@
   input="/etc/swizzin/sources/logo/logo1"
   while IFS= read -r line
   do
-	echo_info  "$line"
+	colorprint "${green}${bold} $1"
   done < "$input"
   /usr/local/bin/swizzin/remove/tools.sh
   /usr/local/bin/swizzin/install/tools.sh
