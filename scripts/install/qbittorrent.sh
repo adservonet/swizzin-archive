8349e23fdeaa2efbe026888390a4fdcd0296e7e6
illnesse <illnesse@gmail.com>
2020-11-27 11:30:46 +0100

    old qbit code

--- scripts/install/qbittorrent.sh
+++ scripts/install/qbittorrent.sh
@@ -5,60 +5,36 @@
 user=$(cut -d: -f1 < /root/.master.info)
 port=$(cat /home/seedit4me/.qbittorrent_port)
 
-
-# Source the required functions
-. /etc/swizzin/sources/functions/qbittorrent
-. /etc/swizzin/sources/functions/libtorrent
-. /etc/swizzin/sources/functions/utils
-. /etc/swizzin/sources/functions/fpm
-
-#users=($(_get_user_list))
-
-#if [[ -n $1 ]]; then
-    user=$1
-    qbittorrent_user_config ${user}
-    if [[ -f /install/.nginx.lock ]]; then
-        echo_progress_start "Configuring nginx"
-        bash /etc/swizzin/scripts/nginx/qbittorrent.sh
-        systemctl reload nginx
-        echo_progress_done
-    fi
-    exit 0
-#fi
-
-repov=$(get_candidate_version qbittorrent-nox)
-latestv41=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 -oP '4\.1\.\d')
-if [[ -z $latestv41 ]]; then latestv41=4.1.9; fi
-latestv42=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 -oP '4\.2\.\d')
-if [[ -z $latestv41 ]]; then latestv42=4.2.5; fi
-latestv=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 .tar.gz | grep -oP '\d.\d?.?\d')
-latestv=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 .tar.gz | grep -oP '\d.\d?.?\d')
-export qbittorrent=${latestv}
-#whiptail_qbittorrent
-check_client_compatibility
-if ! skip_libtorrent_rasterbar; then
-    whiptail_libtorrent_rasterbar
-    echo_progress_start "Building libtorrent-rasterbar"
-    build_libtorrent_rasterbar
-    echo_progress_done "Build completed"
-fi
-
-echo_progress_start "Building qBittorrent"
-build_qbittorrent
-echo_progress_done
-
 if [[ ! -d /home/seedit4me/torrents/qbittorrent ]]; then
   mkdir -p /home/seedit4me/torrents/qbittorrent
   chown -R seedit4me:seedit4me /home/seedit4me/torrents/qbittorrent
   chmod -R 775 /home/seedit4me/torrents/qbittorrent
 fi
-qbittorrent_service
-for user in ${users[@]}; do
-    echo_progress_start "Enabling qbittorrent for $user"
-    qbittorrent_user_config ${user}
-    systemctl enable -q --now qbittorrent@${user} 2>&1  | tee -a $log
-    echo_progress_done "Started qbt for $user"
-done
+
+apt_update
+add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable >>  "${log}"  2>&1
+apt_install qbittorrent-nox >>  "${log}"  2>&1
+adduser --system --group qbittorrent-nox >>  "${log}"  2>&1
+usermod -a -G seedit4me qbittorrent-nox >>  "${log}"  2>&1
+usermod -a -G qbittorrent-nox seedit4me >>  "${log}"  2>&1
+
+cat > /etc/systemd/system/qbittorrent.service <<SSS
+[Unit]
+Description=qBittorrent Command Line Client
+After=network.target
+
+[Service]
+#Do not change to "simple"
+Type=forking
+User=qbittorrent-nox
+Group=qbittorrent-nox
+UMask=007
+ExecStart=/usr/bin/qbittorrent-nox -d --webui-port=9148
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+SSS
 
 
 systemctl enable qbittorrent >>  "${log}"  2>&1
