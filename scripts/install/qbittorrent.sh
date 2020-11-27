54015c5ba6b39dbf39eaa3800925acacbada8c22
illnesse <illnesse@gmail.com>
2020-11-27 10:00:14 +0100

    qbit

--- scripts/install/qbittorrent.sh
+++ scripts/install/qbittorrent.sh
@@ -2,19 +2,16 @@
 # qBittorrent Installer for swizzin
 # Author: liara
 
-user=$(cut -d: -f1 < /root/.master.info)
 port=$(cat /home/seedit4me/.qbittorrent_port)
-
-
 # Source the required functions
 . /etc/swizzin/sources/functions/qbittorrent
 . /etc/swizzin/sources/functions/libtorrent
 . /etc/swizzin/sources/functions/utils
 . /etc/swizzin/sources/functions/fpm
 
-#users=($(_get_user_list))
+users=($(_get_user_list))
 
-#if [[ -n $1 ]]; then
+if [[ -n $1 ]]; then
     user=$1
     qbittorrent_user_config ${user}
     if [[ -f /install/.nginx.lock ]]; then
@@ -24,16 +21,19 @@ port=$(cat /home/seedit4me/.qbittorrent_port)
         echo_progress_done
     fi
     exit 0
-#fi
+fi
 
-repov=$(get_candidate_version qbittorrent-nox)
-latestv41=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 -oP '4\.1\.\d')
-if [[ -z $latestv41 ]]; then latestv41=4.1.9; fi
-latestv42=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 -oP '4\.2\.\d')
-if [[ -z $latestv41 ]]; then latestv42=4.2.5; fi
-latestv=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 .tar.gz | grep -oP '\d.\d?.?\d')
-latestv=$(curl -s https://github.com/qbittorrent/qBittorrent/releases | grep -m1 .tar.gz | grep -oP '\d.\d?.?\d')
-export qbittorrent=${latestv}
+  . /etc/swizzin/sources/functions/apt
+  codename=$(lsb_release -cs)
+  repov=$(get_candidate_version qbittorrent-nox)
+  releases=$(git ls-remote -t --refs https://github.com/qbittorrent/qBittorrent.git | awk '{sub("refs/tags/release-", ""); print $2 }' | sort -r)
+
+  latestv41=$(echo "$releases" | grep -m1 -oP '4\.1\.\d?.?\d')
+  latestv42=$(echo "$releases" | grep -m1 -oP '4\.2\.\d?.?\d')
+  latestv=$(echo "$releases" | grep -m1 -oP '\d.\d?.?\d?.?\d')
+
+  export qbittorrent=${latestv}
+
 #whiptail_qbittorrent
 check_client_compatibility
 if ! skip_libtorrent_rasterbar; then
@@ -72,17 +72,13 @@ echo "Bittorrent\LSD=false" >> /home/qbittorrent-nox/.config/qBittorrent/qBittor
 echo "Bittorrent\PeX=false" >> /home/qbittorrent-nox/.config/qBittorrent/qBittorrent.conf
 echo "WebUI\LocalHostAuth=false" >> /home/qbittorrent-nox/.config/qBittorrent/qBittorrent.conf
 echo "Downloads\SavePath=/home/seedit4me/torrents/qbittorrent/" >> /home/qbittorrent-nox/.config/qBittorrent/qBittorrent.conf
-
-#echo "WebUI\Username=seedit4me" >> /home/qbittorrent-nox/.config/qBittorrent/qBittorrent.conf
-#echo "WebUI\Password_PBKDF2="@ByteArray(Nhc0IAtfyl49psuYV+7BoA==:WyEqotj1k7/5x6dgqv9lUKo7Ez69Lqh8CxstajGgi+DwrdwUnZiDEwbK97zhZJB+c6SKlKPVsWq3uxYAS54dNA==)"
-
 systemctl restart qbittorrent >>  "${log}"  2>&1
 
 if [[ -f /install/.nginx.lock ]]; then
     echo_progress_start "Configuring nginx"
     bash /etc/swizzin/scripts/nginx/qbittorrent.sh
     systemctl reload nginx >> $log 2>&1
-    echo_progress_done 
+    echo_progress_done
 fi
 
 touch /install/.qbittorrent.lock
