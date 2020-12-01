330d2760b9a60dc20689e131883118ee921ff6ba
illnesse <illnesse@gmail.com>
2020-12-01 22:06:24 +0100

    radarr temp fix before big deploy

--- scripts/update/radarr.sh
+++ scripts/update/radarr.sh
@@ -7,6 +7,7 @@ password=$(cut -d: -f2 < /root/.master.info)
 
 if [[ -f /install/.radarr.lock ]]; then
 
+  apt install jq
 	#Move v3mono installs to v3.net
 	if grep -q "ExecStart=/usr/bin/mono" /etc/systemd/system/radarr.service; then
 		echo "Moving Radarr from mono to .Net"
