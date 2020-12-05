a3a42c392c7da2ab5eb1b274226c6c1601d2ecbb
illnesse <illnesse@gmail.com>
2020-12-05 23:31:22 +0100

    medusa

--- scripts/remove/medusa.sh
+++ scripts/remove/medusa.sh
@@ -2,6 +2,16 @@
 # Medusa Uninstaller for Swizzin
 # Author: liara
 
+user=$(cut -d: -f1 < /root/.master.info)
+systemctl disable medusa@${user}
+systemctl stop medusa@${user}
+sudo rm /etc/nginx/apps/medusa.conf > /dev/null 2>&1
+sudo rm /etc/systemd/medusa@.service > /dev/null 2>&1
+sudo rm /install/.medusa.lock
+rm -rf /home/${user}/.medusa
+
+
+
 systemctl disable --now -q medusa
 
 sudo rm /etc/nginx/apps/medusa.conf > /dev/null 2>&1
