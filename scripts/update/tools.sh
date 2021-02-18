b14d9e9a1f2dca590a879e0da5eb011b488bb09f
illnesse <illnesse@gmail.com>
2021-02-18 20:57:45 +0100

    check if apt_upgrade is a function

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
 #always install tools
-#if [[ -d /srv/tools ]]; then
-  echo "Updating tools"
+echo "Updating tools"
 
-#  apt_upgrade;
+if type apt_upgrade | grep -q '^function$' 2>/dev/null; then
+    apt_upgrade
+else
+  waitforapt
+  sudo apt update >> "${SEEDIT_LOG}"  2>&1;
+  sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y >> "${SEEDIT_LOG}"  2>&1;
+  sudo apt upgrade -y >> "${SEEDIT_LOG}"  2>&1;
+  sudo apt autoremove -y >> "${SEEDIT_LOG}"  2>&1;
+fi
 
-  /usr/local/bin/swizzin/remove/tools.sh
-  /usr/local/bin/swizzin/install/tools.sh
-#fi
+/usr/local/bin/swizzin/remove/tools.sh
+/usr/local/bin/swizzin/install/tools.sh
