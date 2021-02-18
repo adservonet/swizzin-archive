3c060f1f4c0f64f4d7802754d7d5563f1e5bedce
illnesse <illnesse@gmail.com>
2021-02-18 16:34:50 +0100

    apt_upgrade() in box, tools, mono

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -4,13 +4,7 @@
 #if [[ -d /srv/tools ]]; then
   echo "Updating tools"
 
-  export SEEDIT_LOG=/root/logs/install.log
-
-  waitforapt
-  sudo apt update >> "${SEEDIT_LOG}"  2>&1;
-  sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y >> "${SEEDIT_LOG}"  2>&1;
-  sudo apt upgrade -y >> "${SEEDIT_LOG}"  2>&1;
-  sudo apt autoremove -y >> "${SEEDIT_LOG}"  2>&1;
+  apt_upgrade;
 
   /usr/local/bin/swizzin/remove/tools.sh
   /usr/local/bin/swizzin/install/tools.sh
