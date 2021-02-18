f100da5f5932312f15c6d6e5364a1bdb25d79565
illnesse <illnesse@gmail.com>
2021-02-18 21:00:03 +0100

    undo

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -3,15 +3,15 @@
 #always install tools
 echo "Updating tools"
 
-if type apt_upgrade | grep -q '^function$' 2>/dev/null; then
-    apt_upgrade
-else
-  waitforapt
-  sudo apt update >> "${SEEDIT_LOG}"  2>&1;
-  sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y >> "${SEEDIT_LOG}"  2>&1;
-  sudo apt upgrade -y >> "${SEEDIT_LOG}"  2>&1;
-  sudo apt autoremove -y >> "${SEEDIT_LOG}"  2>&1;
-fi
+#if type apt_upgrade | grep -q '^function$' 2>/dev/null; then
+#    apt_upgrade
+#else
+#  waitforapt
+#  sudo apt update >> "${SEEDIT_LOG}"  2>&1;
+#  sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y >> "${SEEDIT_LOG}"  2>&1;
+#  sudo apt upgrade -y >> "${SEEDIT_LOG}"  2>&1;
+#  sudo apt autoremove -y >> "${SEEDIT_LOG}"  2>&1;
+#fi
 
 /usr/local/bin/swizzin/remove/tools.sh
 /usr/local/bin/swizzin/install/tools.sh
