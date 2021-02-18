3731cb59208f426181e5b303d615a6f455da7655
illnesse <illnesse@gmail.com>
2021-02-18 15:28:50 +0100

    fixed oldschool box update apt upgrade

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -4,6 +4,13 @@
 #if [[ -d /srv/tools ]]; then
   echo "Updating tools"
 
+
+   waitforapt
+   sudo apt update
+   sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y;
+   sudo apt upgrade -y
+   sudo apt autoremove
+
   /usr/local/bin/swizzin/remove/tools.sh
   /usr/local/bin/swizzin/install/tools.sh
 #fi
