69f49b91f8f7c9762231afa71a20b2beeb5a780b
illnesse <illnesse@gmail.com>
2021-02-18 16:25:01 +0100

    apt update to log as well

--- scripts/update/tools.sh
+++ scripts/update/tools.sh
@@ -7,7 +7,7 @@
   export SEEDIT_LOG=/root/logs/install.log
 
   waitforapt
-  sudo apt update
+  sudo apt update >> "${SEEDIT_LOG}"  2>&1;
   sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y >> "${SEEDIT_LOG}"  2>&1;
   sudo apt upgrade -y >> "${SEEDIT_LOG}"  2>&1;
   sudo apt autoremove -y >> "${SEEDIT_LOG}"  2>&1;
