9f13dcffe78c38a25047b5d051156f90f14450d3
illnesse <illnesse@gmail.com>
2021-02-19 17:51:38 +0100

    proftpd remove .sh

---
+++ scripts/remove/proftpd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -f /install/.tools.lock ]]; then
+    export log=/srv/tools/logs/output.log
+else
+    export log=/root/logs/install.log
+fi
+
+apt_remove proftpd proftpd-basic >> "${log}" 2>&1
+
+rm /install/.proftpd.lock
+
+
