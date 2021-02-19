9793912c995c73ac1a040fd0e6faff574bb866cc
illnesse <illnesse@gmail.com>
2021-02-19 03:10:32 +0100

    cp conf

--- scripts/nginx/couchpotato.sh
+++ scripts/nginx/couchpotato.sh
@@ -23,7 +23,8 @@ location /couchpotato {
 }
 RAD
 fi
-sed -i "s/url_base.*/url_base = couchpotato\nhost = 127.0.0.1/g" /home/${user}/.config/couchpotato/settings.conf
+#sed -i "s/url_base.*/url_base = couchpotato\nhost = 127.0.0.1/g" /home/${user}/.config/couchpotato/settings.conf
+sed -i 's/url_base.*/url_base = \/couchpotato/g' /home/${user}/.config/couchpotato/settings.conf >> "${log}" 2>&1
 if [[ $isactive == "active" ]]; then
     systemctl start couchpotato
 fi
