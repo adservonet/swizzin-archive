07c108ab46d0786ee0d87ca4489ab25226cda666
illnesse <illnesse@gmail.com>
2021-02-19 11:55:25 +0100

    emby symlink to home dir

--- scripts/remove/emby.sh
+++ scripts/remove/emby.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Removal script for emby
 
+master=$(cut -d: -f1 < /root/.master.info)
+
 systemctl disable --now -q emby-server
 dpkg -r emby-server > /dev/null 2>&1
 if [[ -f /etc/apt/sources.list.d/emby-server.list ]]; then
     rm /etc/apt/sources.list.d/emby-server.list
 fi
+rm -rf /home/${master}/emby
 rm -rf /etc/nginx/apps/emby.conf
 rm -rf /install/.emby.lock
