1fc53ef20b70f400e19d96bd57e0ac2af3aa0e0e
illnesse <illnesse@gmail.com>
2021-02-22 18:48:13 +0100

    export rtorrentver='0.9.8' export libtorrentver='0.13.8'

--- scripts/install/rtorrent.sh
+++ scripts/install/rtorrent.sh
@@ -91,8 +91,8 @@ if [[ -n $1 ]]; then
     exit 0
 fi
 
-export rtorrentver='repo'
-export libtorrentver=RC_1_2
+export rtorrentver='0.9.8'
+export libtorrentver='0.13.8'
 
 if [[ -n $noexec ]]; then
     mount -o remount,exec /tmp
