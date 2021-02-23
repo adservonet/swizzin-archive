286c88828fe5dedd9c8a9ea48e8f5e6ec28c8e6f
illnesse <illnesse@gmail.com>
2021-02-23 01:01:24 +0100

    redundant

--- scripts/install/qbittorrent.sh
+++ scripts/install/qbittorrent.sh
@@ -2,8 +2,6 @@
 # qBittorrent Installer for swizzin
 # Author: liara
 
-user=$(cut -d: -f1 < /root/.master.info)
-port=$(cat /home/${user}/.qbittorrent_port)
 # Source the required functions
 . /etc/swizzin/sources/functions/qbittorrent
 . /etc/swizzin/sources/functions/libtorrent
@@ -44,11 +42,6 @@ echo_progress_start "Building qBittorrent"
 build_qbittorrent
 echo_progress_done
 
-if [[ ! -d /home/${user}/torrents/qbittorrent ]]; then
-    mkdir -p /home/${user}/torrents/qbittorrent
-    chown -R ${user}:${user} /home/${user}/torrents/qbittorrent
-    chmod -R 775 /home/${user}/torrents/qbittorrent
-fi
 qbittorrent_service
 for user in ${users[@]}; do
     echo_progress_start "Enabling qbittorrent for $user"
@@ -56,21 +49,6 @@ for user in ${users[@]}; do
     systemctl enable -q --now qbittorrent@${user} 2>&1 | tee -a $log
     echo_progress_done "Started qbt for $user"
 done
-
-sleep 10
-for user in ${users[@]}; do
-    sed -i -e 's/Connection\\PortRangeMin=.*/Connection\\PortRangeMin='$port' /g' /home/${user}/.config/qBittorrent/qBittorrent.conf
-    echo "Bittorrent\DHT=false" >> /home/${user}/.config/qBittorrent/qBittorrent.conf
-    echo "Bittorrent\LSD=false" >> /home/${user}/.config/qBittorrent/qBittorrent.conf
-    echo "Bittorrent\PeX=false" >> /home/${user}/.config/qBittorrent/qBittorrent.conf
-    echo "WebUI\LocalHostAuth=false" >> /home/${user}/.config/qBittorrent/qBittorrent.conf
-    echo "Downloads\SavePath=/home/seedit4me/torrents/qbittorrent/" >> /home/${user}/.config/qBittorrent/qBittorrent.conf
-done
-
-for user in ${users[@]}; do
-    systemctl restart -q --now qbittorrent@${user} 2>&1 | tee -a $log
-done
-
 if [[ -f /install/.nginx.lock ]]; then
     echo_progress_start "Configuring nginx"
     bash /etc/swizzin/scripts/nginx/qbittorrent.sh
