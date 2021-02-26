ae7823e4a9b7e96c4059e88b36cdcad1bb61d66b
illnesse <illnesse@gmail.com>
2021-02-26 10:38:58 +0100

    transmission nginx fix

--- scripts/nginx/transmission.sh
+++ scripts/nginx/transmission.sh
@@ -5,24 +5,51 @@
 
 users=($(cut -d: -f1 < /etc/htpasswd))
 
+#if [[ ! -f /etc/nginx/apps/transmission.conf ]]; then
+#    cat > /etc/nginx/apps/transmission.conf << TCONF
+#location /transmission {
+#    return 301 /transmission/web/;
+#}
+#
+#location /transmission/ {
+#    proxy_set_header X-Real-IP \$remote_addr;
+#    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+#    proxy_set_header Host \$http_host;
+#    proxy_set_header X-NginX-Proxy true;
+#    proxy_http_version 1.1;
+#    proxy_set_header Connection "";
+#    proxy_pass_header X-Transmission-Session-Id;
+#    add_header   Front-End-Https   on;
+#    auth_basic "What's the password?";
+#    auth_basic_user_file /etc/htpasswd;
+#    proxy_pass http://\$remote_user.transmission;
+#}
+#TCONF
+#fi
+
 if [[ ! -f /etc/nginx/apps/transmission.conf ]]; then
     cat > /etc/nginx/apps/transmission.conf << TCONF
 location /transmission {
-    return 301 /transmission/web/;
-}
+    include /etc/nginx/snippets/proxy.conf;
 
-location /transmission/ {
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header Host \$http_host;
-    proxy_set_header X-NginX-Proxy true;
-    proxy_http_version 1.1;
-    proxy_set_header Connection "";
-    proxy_pass_header X-Transmission-Session-Id;
-    add_header   Front-End-Https   on;
+    proxy_pass_header  X-Transmission-Session-Id;
+    proxy_set_header   X-Forwarded-Host   $host;
+    proxy_set_header   X-Forwarded-Server $host;
+    proxy_set_header   X-Forwarded-For    $proxy_add_x_forwarded_for;
+
+    proxy_pass        http://127.0.0.1:9091/transmission/web;
     auth_basic "What's the password?";
     auth_basic_user_file /etc/htpasswd;
-    proxy_pass http://\$remote_user.transmission;
+}
+
+location /rpc {
+    proxy_pass_header  X-Transmission-Session-Id;
+    proxy_pass         http://127.0.0.1:9091/transmission/rpc;
+}
+
+location /upload {
+    proxy_pass_header  X-Transmission-Session-Id;
+    proxy_pass         http://127.0.0.1:9091/transmission/upload;
 }
 TCONF
 fi
