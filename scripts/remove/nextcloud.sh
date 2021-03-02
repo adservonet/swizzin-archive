5a82bdfe37237211217807a9af70254d9dc0be1b
illnesse <illnesse@gmail.com>
2021-03-02 20:22:14 +0100

    purge php7.3* on nextcloud remove

--- scripts/remove/nextcloud.sh
+++ scripts/remove/nextcloud.sh
@@ -5,6 +5,7 @@
 password=$(cut -d: -f2 < /root/.master.info)
 rm -rf /srv/nextcloud
 rm /etc/nginx/apps/nextcloud.conf
+apt_remove purge php7.3*
 systemctl reload nginx
 host=$(mysql -u root --password="$password" --execute="select host from mysql.user where user = 'nextcloud';" | grep -E "localhost|127.0.0.1")
 mysql --user="root" --password="$password" --execute="DROP DATABASE nextcloud;"
