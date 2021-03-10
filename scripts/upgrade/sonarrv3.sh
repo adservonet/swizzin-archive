83c0c5a1968e9e63d8deaa58a0d7827a01c75a9b
illnesse <illnesse@gmail.com>
2021-03-10 23:52:59 +0100

    v3 zZzz

--- scripts/upgrade/sonarrv3.sh
+++ scripts/upgrade/sonarrv3.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ ! -f /install/.sonarr.lock ]]; then
+if [[ ! -f /install/.sonarrv3.lock ]]; then
     echo_error "Sonarr not detected. Exiting!"
     exit 1
 else
-    rm /install/.sonarr.lock
+    rm /install/.sonarrv3.lock
+    box install sonarrv3
     box remove sonarrv3
     box install sonarrv3
 fi
