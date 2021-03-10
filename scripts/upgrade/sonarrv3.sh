dc8a8bfef0aaf37ba63988d9251ab714094fb7fb
illnesse <illnesse@gmail.com>
2021-03-11 00:19:48 +0100

    keep settings

--- scripts/upgrade/sonarrv3.sh
+++ scripts/upgrade/sonarrv3.sh
@@ -5,8 +5,11 @@ if [[ ! -f /install/.sonarrv3.lock ]]; then
     exit 1
 else
     rm /install/.sonarrv3.lock
+    cp /home/seedit4me/.config/sonarr/config.xml /tmp
     /usr/local/bin/swizzin/install/sonarrv3.sh
     /usr/local/bin/swizzin/remove/sonarrv3.sh
     /usr/local/bin/swizzin/install/sonarrv3.sh
+    cp /tmp/config.xml /home/seedit4me/.config/sonarr
+    systemctl restart sonarr
 fi
 
