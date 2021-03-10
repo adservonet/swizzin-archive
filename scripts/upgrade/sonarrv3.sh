9dbe15c2f3229ff522a87256f0d3710cf1a92cd7
illnesse <illnesse@gmail.com>
2021-03-11 00:55:13 +0100

    keep db too

--- scripts/upgrade/sonarrv3.sh
+++ scripts/upgrade/sonarrv3.sh
@@ -4,12 +4,16 @@ if [[ ! -f /install/.sonarrv3.lock ]]; then
     echo_error "Sonarr not detected. Exiting!"
     exit 1
 else
+    systemctl stop sonarr
+    rm -rf /tmp/sonarrv3
+    mkdir -p /tmp/sonarrv3
+    cp -rp /home/seedit4me/.config/sonarr/* /tmp/sonarrv3
     rm /install/.sonarrv3.lock
-    cp /home/seedit4me/.config/sonarr/config.xml /tmp
     /usr/local/bin/swizzin/install/sonarrv3.sh
     /usr/local/bin/swizzin/remove/sonarrv3.sh
     /usr/local/bin/swizzin/install/sonarrv3.sh
-    cp /tmp/config.xml /home/seedit4me/.config/sonarr
-    systemctl restart sonarr
+    /bin/cp -rfp /tmp/sonarrv3/* /home/seedit4me/.config/sonarr
+    rm -rf /tmp/sonarrv3
+    systemctl start sonarr
 fi
 
