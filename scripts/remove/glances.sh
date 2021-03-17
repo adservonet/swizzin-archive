0a64d9d6e0368e4dabfa3fa2f8904bb00b9b2c27
illnesse <illnesse@gmail.com>
2021-03-17 14:24:41 +0100

    remove glances

---
+++ scripts/remove/glances.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Remove glances from swizzin
+#
+
+systemctl disable --now -q glances
+
+if [[ -f /install/.nginx.lock ]]; then
+    rm -f /etc/nginx/apps/glances.conf
+    systemctl reload nginx
+fi
+
+apt_remove glances
+
+rm /install/.glances.lock
