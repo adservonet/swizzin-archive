de3de4aaa83b9faef3e48b19ffb6a851a32e90b7
illnesse <illnesse@gmail.com>
2021-04-26 03:44:34 +0200

    restart services

--- scripts/tools/updateports.sh
+++ scripts/tools/updateports.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+user=$(cut -d: -f1 < /root/.master.info)
 
 app="rtorrent"
 if [[ -f /install/.$app.lock ]]; then
   echo_info "updating ports for ${app}."
-  user=$(cut -d: -f1 < /root/.master.info)
   port=$(cat /home/seedit4me/.rtorrent_port)
   portend=$(cat /home/seedit4me/.rtorrent_port)
   sed -i "s/network.port_range.set.*/network.port_range.set = ${port}-${portend}/g" /home/${user}/.rtorrent.rc
+  systemctl restart rtorrent@${user}
   echo_success "${app} ports updated."
 fi
