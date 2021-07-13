deb681c0004d1337aa683b21784cc529058fb984
illnesse <illnesse@gmail.com>
2021-07-13 20:45:18 +0200

    box remove sonarr box install sonarr

--- scripts/upgrade/sonarr.sh
+++ scripts/upgrade/sonarr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ ! -f /install/.sonarrold.lock ]]; then
-    echo_error "Sonarr v2 not detected. Exiting!"
-    exit 1
-fi
-
+#if [[ ! -f /install/.sonarrold.lock ]]; then
+#    echo_error "Sonarr v2 not detected. Exiting!"
+#    exit 1
+#fi
+box remove sonarr
 box install sonarr
