cb73cdd4a9536441e0814b5c5130026de1f4da7e
root@root.fail <root@root.fail>
2022-06-06 20:40:36 +0200

    removed vnstat from deps

--- scripts/update/10-dependencies.sh
+++ scripts/update/10-dependencies.sh
@@ -39,6 +39,6 @@ if [[ $trigger_apt_update == "true" ]]; then
 fi
 
 #space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
-dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools gnupg2 cracklib-runtime unzip ccze"
+dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools gnupg2 cracklib-runtime unzip ccze"
 
 apt_install "${dependencies[@]}"
