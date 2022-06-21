8b61e011527bf4d7d5264cc01d62288d7f95f0f2
root@root.fail <root@root.fail>
2022-06-21 14:57:43 +0200

    apt_update

--- scripts/install/unpackerr.sh
+++ scripts/install/unpackerr.sh
@@ -3,6 +3,7 @@
 if [ -d /etc/apt/sources.list.d ]; then
   curl -sL https://packagecloud.io/golift/pkgs/gpgkey | apt-key add -
   echo "deb https://packagecloud.io/golift/pkgs/ubuntu focal main" > /etc/apt/sources.list.d/golift.list
+  apt_update
   apt_install unpackerr
 fi
 
